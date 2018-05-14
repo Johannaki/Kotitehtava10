dbd99fc75f7a5a39dda670c7902dd058c48f774a
Johanna Kinnari <johanna.kinnari@metropolia.fi>
2018-05-14 15:18:45 +0300

    Apua

---
+++ kotitehtava10.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+     <meta charset="UTF-8">
+       <title></title>
+       <style></style>
+     </head>
+     <body>
+
+       <h2>Mitä haluat tehdä?</h2>
+
+       <form action="kotitehtava10.php" method="POST">
+         <input type="radio" name="toiminto" value="Nosto" checked> Nostaa rahaa tililtä<br>
+         <input type="radio" name="toiminto" value="Pano"> Lisätä rahaa tilille<br>
+         <input type="radio" name="toiminto" value="Uusi"> Avata tili<br>
+         <input type="radio" name="toiminto" value="Heippa"> Sulkea tili<br>
+         <input type="submit" value="Toteuta toiminto"><br><br>
+       </form>
+
+
+<?php
+
+
+//Luokka
+  class BankAccount {
+    public $accountNumber;
+    private $balance;
+    public $dateOpened;
+    public $accountType;
+
+    //metodi
+  //En tiedä saisiko näin tehdä, mutta panisin tuon ehdon tähän koodin malliin.
+  if ((isset($_POST['submit']) && $_POST['toiminto'] == 'true')) {
+  public function set_a() {
+    $this->accountNumber = $newaccountNumber;
+    $this->balance = $newbalance;
+    $this->dateOpened = $newdateOpened;
+    $this->accountType = $newaccountType;
+  }
+  function puhu() {
+    echo "$this->newaccountNumber <br>$this->newbalance <br>$this->newdateOpened <br>$this->newaccountType<br><br>";
+  }
+
+  } else {
+    function __construct($accountNumber, $balance, $dateOpened, $accountType) {
+      $this->accountNumber = $accountNumber;
+      $this->balance = $balance;
+      $this->dateOpened = $dateOpened;
+      $this->accountType = $accountType;
+    }
+  }
+
+    function puhu() {
+      echo "$this->accountNumber <br>$this->balance <br>$this->dateOpened <br>$this->accountType<br><br>";
+    }
+}
+
+
+//Olio
+$A7652= new BankAccount("A7652", "$500", "5/3/2000", "Cheking");
+$B2311= new BankAccount("B2311", "-$50", "1/2/2012", "Cheking");
+$S2314= new BankAccount("S2314", "$7500", "1/2/1994", "Saving");
+
+
+if ((isset($_POST['submit']) && $_POST['toiminto'] == 'true')) {
+  $A7652->set_a();
+  $B2311->set_a();
+  $S2314->set_a();
+} else {
+$A7652->puhu();
+$B2311->puhu();
+$S2314->puhu();
+}
+
+
+?>
+
+</body>
+</html>
